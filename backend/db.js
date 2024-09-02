cac4c550c5254e897678d4afffc80ed6e479789e
Himanshu2025 <hkulkarni927@gmail.com>
2024-09-02 11:35:14 +1000

    Payment application

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
-// backend/db.js
+
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/paytm")
+mongoose.connect("mongodb+srv://hkulkarni927:12345@cluster0.m1yhhx6.mongodb.net/")
 
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
